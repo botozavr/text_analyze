b40e69f6e14d212f91a84231d90a291af500cc1f
Botozavr <kronshtein01@gmail.com>
2026-02-02 18:09:23 +0300

    Анализатор текста из файла. Первая версия

---
+++ text_utils.py
@@ -0,0 +1,35 @@
+"""
+1.	Написать модуль text_utils.py с функциями:
+o	очистка текста от знаков препинания,
+o	разбиение на слова,
+o	подсчёт частоты слов (возврат dict).
+2.	Второй модуль main.py, который:
+o	читает текст из файла,
+o	использует функции text_utils,
+o	печатает топ 10 слов.
+"""
+
+#text_utils.py
+
+from re import sub
+from typing import List
+
+def clear_text(my_str: str) -> str:
+    """Очищает текст от знаков препинания"""
+    return sub(r'[^\w\s]', '', my_str)
+
+def sep_words (my_str: str) -> List[str]:
+    """"Разделяет текст на слова"""
+    return my_str.split()
+
+def count_words(words: List[str]) -> int:
+    """Подсчёт частоты слов"""
+    x = {}
+    for i in words:
+        if i in x:
+            x[i]+=1
+        else:
+            x[i] = 1
+    return x
+
+
