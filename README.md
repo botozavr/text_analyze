ca2cfc683cff6d066044ac337ba82e43a2bcb21a
Botozavr <kronshtein01@gmail.com>
2026-02-03 08:34:23 +0300

    Добавлен аргумент командной строки --top - количество выводимых топ-фалов,+ небольшие правки и дополнения

---
+++ README.md
@@ -0,0 +1,12 @@
+usage: wordfreq.py [-h] [-t TOP] input_file
+
+Анализатор текстовых файлов
+
+positional arguments:
+  input_file         Путь к файлу для анализа
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -t TOP, --top TOP  Количество топ-слов для вывода (по умолчанию: 10)
+
+Пример использования: python -m wordfreq.py path/to/file.txt --top 20
---
+++ wordfreq.py
@@ -0,0 +1,72 @@
+#wordfreq.py
+
+import sys
+from dataclasses import dataclass
+import argparse
+from typing import List, Tuple
+
+from text_utils import clear_text, sep_words, count_words
+
+
+@dataclass
+class DocumentStats:
+    path: str   #путь к файлу
+    word_count: int #число слов в тексте
+    top_words: List[Tuple[str, int]] #топ слов
+
+def analyze_file(path: str, top: int = 10) -> DocumentStats:
+    """Анализ файла
+    path - путь к файлу
+    DocumentStats - итоги анализа"""
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            text = file.read()
+    except Exception as e:
+        print(f"Ошибка: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+    dict = count_words(sep_words(clear_text(text)))
+    sorted_dict = {}
+    for key in sorted(dict, key=dict.get, reverse=True):
+        sorted_dict[key] = dict[key]
+    return DocumentStats(path = path, word_count = len(dict), top_words = list(sorted_dict.items())[:top])
+
+def main():
+    if len(sys.argv) < 2:
+        print("Требуется указать файл")
+        sys.exit(1)  # Выход с ошибкой
+
+    parser = argparse.ArgumentParser(
+        description="Анализатор текстовых файлов",
+        epilog="Пример использования: python -m wordfreq.py path/to/file.txt --top 20"
+    )
+
+    # Обязательный аргумент
+    parser.add_argument(
+        "input_file",
+        help="Путь к файлу для анализа"
+    )
+    parser.add_argument(
+        "-t",
+        "--top",
+        type=int,
+        default = 10,
+        help="Количество топ-слов для вывода (по умолчанию: 10)"
+    )
+
+    args = parser.parse_args()
+
+    result = analyze_file(args.input_file, args.top)
+    print(f"-" * 30)
+    print('Анализ файла')
+    print(f"-" * 30)
+    print(f"Число слов: {result.word_count}")
+    print(f"-" * 30)
+    print("топ-10 слов: \n")
+    for i, (word, count) in enumerate(result.top_words, 1):  # 1 - начинаем с 1
+        print(f"{i:2}. Слово '{word}' встречается\t {count:4} раз(а)")
+    print(f"-" * 30)
+
+
+main()
